102be0b9144502923732c54996f97476054f84bd
wuhkcoder <15757129476@163.com>
2016-05-21 22:40:36 +0800

    1、异常情况下Activity生命周期

---
+++ DevArt/app/src/main/java/com/wuhk/devart/MainActivity.java
@@ -0,0 +1,33 @@
+package com.wuhk.devart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null != savedInstanceState){
+            String test = savedInstanceState.getString("extra_test");
+            Log.e(TAG , "[onCreate]restore extra_test:" + test);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.e(TAG, "onSaveInstanceState");
+        outState.putString("extra_test" , "test");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String test = savedInstanceState.getString("extra_test");
+        Log.e(TAG , "[onRestoreInstanceState]restore extra_test:" + test);
+    }
+}
