4ca28b7b8bb86d4ae10341fe447cd711123e28e1
wuhkcoder <15757129476@163.com>
2016-05-21 22:56:39 +0800

    1、bb

---
+++ DevArt/app/src/main/java/com/wuhk/devart/App.java
@@ -0,0 +1,7 @@
+package com.wuhk.devart;
+
+/**
+ * Created by wuhk on 2016/5/21.
+ */
+public class App {
+}
--- DevArt/app/src/main/java/com/wuhk/devart/chapter1/LifeCycleActivity.java
+++ DevArt/app/src/main/java/com/wuhk/devart/chapter1/LifeCycleActivity.java
@@ -1,11 +1,13 @@
-package com.wuhk.devart;
+package com.wuhk.devart.chapter1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "MainActivity";
+import com.wuhk.devart.R;
+
+public class LifeCycleActivity extends AppCompatActivity {
+    public static final String TAG = "LifeCycleActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
