a038a83654c81d44570f86bbede7d08339ed0dff
wuhkcoder <15757129476@163.com>
2016-05-21 22:59:30 +0800

    1、bb

--- DevArt/app/src/main/java/com/wuhk/devart/chapter1/LifeCycleActivity.java
+++ DevArt/app/src/main/java/com/wuhk/devart/chapter1/LifeCycleActivity.java
@@ -9,8 +9,6 @@ import com.wuhk.devart.R;
 public class LifeCycleActivity extends AppCompatActivity {
     public static final String TAG = "LifeCycleActivity";
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
